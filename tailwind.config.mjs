9fe082b1fa632e4d74dc350f48de1c729ca5a3ce
Adam Levoy <ajlevoy@gmail.com>
2024-03-14 10:02:49 -0700

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'hero-desktop': "url('./src/assets/images/hero-image-desktop.jpg')"
+			}
+		}
 	},
-	plugins: [],
-}
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#BA0C2F', // red
+					secondary: '#DDDDDD', // gray
+					accent: '#8065b1', // purple
+					neutral: '#000000', // black
+					'base-100': '#ffffff' // white
+				}
+			}
+		]
+	}
+};
